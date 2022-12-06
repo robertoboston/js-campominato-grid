383e3e16f1baf2c05aedec9fc489124359e85dea
roberto.boston <roberto.bozzo97@tiscali.it>
2022-12-06 16:59:44 +0100

    select created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 const playBtn = document.getElementById('btn')
 
+let diff_1 = document.getElementById('difficolta-1').value
+let diff_2 = document.getElementById('difficolta-2').value
+let diff_3 = document.getElementById('difficolta-3').value
+
 playBtn.addEventListener('click', function(){
     let grid= document.getElementById('grid')
     let divItem;
