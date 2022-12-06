3110b2afecca01ace453f3cff4f8731cb83c2ade
roberto.boston <roberto.bozzo97@tiscali.it>
2022-12-06 16:04:59 +0100

    button created

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 
-let grid= document.getElementById('grid')
-let divItem;
+const playBtn = document.getElementById('btn')
 
-function createDiv (){
+playBtn.addEventListener('click', function(){
+    let grid= document.getElementById('grid')
+    let divItem;
+
     for(let i = 1; i<=100; i++){
         divItem = document.createElement('div')
         
         grid.appendChild(divItem)
     
         divItem.classList.add('square')
+
+        divItem.innerText=i
     
-        divItem.innerText= [i]
+
+        divItem.addEventListener('click',function(){
+            this.classList.add('azure')
+            console.log(this.innerText)
+            
+        })
         
     }
 
-}
+})
 
-createDiv()
+
+
 
 
 
